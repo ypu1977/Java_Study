edbfaaef47ea14e514053e15bde17d8420eba6ff
ypu1977 <ypu1977@naver.com>
2022-07-08 10:41:14 +0900

    금요일

--- src/com/iu/loop/Loop4_ex2.java
+++ src/com/iu/loop/Loop4_ex2.java
@@ -10,7 +10,37 @@ public class Loop4_ex2 {
 		//탄창 : 30발, 3개
 		
 		
-		//1. 단발 2. 점사
+//		//1. 단발 2. 점사
+//		Scanner sc = new Scanner(System.in);
+//		for(int mag = 0; mag<3; mag++) {
+//			System.out.println("1.단발 2.점사");
+//			int select=sc.nextInt();
+//			int count=10;
+//			String sound="타타탕";
+//			if(select==1) {
+//				count = 30;
+//				sound ="탕";
+//			}
+//			for(int b=0; b<count; b++) {
+//				System.out.println(sound);
+//			}
+//		}
+		
+//		System.out.println();
+//		int select=sc.nextInt("1.단발 2.점사");
+//		
+//		int inc = 3;
+//		String sound="타타탕";
+//		
+//		if(select==1) {
+//			inc = 1;
+//			sound = "탕";
+//		}
+//		
+//		for(int b=0;b<30;b=b+inc) {
+//			System.out.println(sound);
+//		}
+		
 		Scanner sc = new Scanner(System.in);
 	      for (int i=0; i<3; i++) {
 	         System.out.println("1번 or 2번 모드를 선택해주세요");
